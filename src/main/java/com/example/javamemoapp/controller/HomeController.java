016bc6337005757852cecd08383f003e93b29a81
daichi taguchi <the.earth3121@gmail.com>
2026-07-05 09:52:19 +0000

    add:backend @PathVariableを体系的に学習。パスパラメータから値を受け取り動的に返却する。

--- src/main/java/com/example/javamemoapp/controller/HomeController.java
+++ src/main/java/com/example/javamemoapp/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.javamemoapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -21,5 +22,13 @@ public class HomeController {
             Model model) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @GetMapping("/users/{id}")
+    public String user(
+            @PathVariable Long id,
+            Model model) {
+        model.addAttribute("id", id);
+        return "user";
     }
 }
