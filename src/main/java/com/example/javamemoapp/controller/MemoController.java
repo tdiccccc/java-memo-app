ce4a438e28579a875458e5a9b9047a56bfb0a727
daichi taguchi <the.earth3121@gmail.com>
2026-07-05 10:25:11 +0000

    add:backend バリデーションを体系的に学習 - @NotBlank 必須項目化 - @Size 文字数制限
    - @Valid
    入力チェックの命令
    
    - BindingResult
    MemoFormに記載のバリデーションが全て入っている。

--- src/main/java/com/example/javamemoapp/controller/MemoController.java
+++ src/main/java/com/example/javamemoapp/controller/MemoController.java
@@ -1,8 +1,12 @@
 package com.example.javamemoapp.controller;
 
 import com.example.javamemoapp.form.MemoForm;
+
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +21,14 @@ public class MemoController {
     }
 
     @PostMapping("/memos")
-    public String create(@ModelAttribute MemoForm memoForm, Model model) {
+    public String create(
+            @Valid @ModelAttribute("memoForm") MemoForm memoForm,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "memos/new";
+        }
         model.addAttribute("memo", memoForm);
         return "memos/show";
     }
--- src/main/java/com/example/javamemoapp/form/MemoForm.java
+++ src/main/java/com/example/javamemoapp/form/MemoForm.java
@@ -1,7 +1,15 @@
 package com.example.javamemoapp.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class MemoForm {
+    @NotBlank(message = "タイトルは必須です")
+    @Size(max = 100, message = "タイトルは100文字以内で入力してください")
     private String title;
+
+    @NotBlank(message = "内容は必須です")
+    @Size(max = 1000, message = "内容は1000文字以内で入力してください")
     private String content;
 
     public String getTitle() {
