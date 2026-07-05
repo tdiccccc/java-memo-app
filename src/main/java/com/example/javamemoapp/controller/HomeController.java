c0fa525bc3c5d88446dab65dbdd65670c0630acd
daichi taguchi <the.earth3121@gmail.com>
2026-07-05 09:56:32 +0000

    add:backend ポストリクエストを体系的に学習。フォーム画面からリクエスト送信して結果画面遷移まで

--- src/main/java/com/example/javamemoapp/controller/HomeController.java
+++ src/main/java/com/example/javamemoapp/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -30,5 +31,20 @@ public class HomeController {
             Model model) {
         model.addAttribute("id", id);
         return "user";
+    }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submit(
+            @RequestParam String name,
+            Model model) {
+
+        model.addAttribute("name", name);
+
+        return "result";
     }
 }
