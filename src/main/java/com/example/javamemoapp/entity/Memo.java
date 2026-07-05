5e1c4d7db9d0fdc5fc2e069de918c77438a3820d
daichi taguchi <the.earth3121@gmail.com>
2026-07-05 11:58:24 +0000

    fix:backend ルールをEntityに閉じ込める。Memoの作成のルールを閉じ込めることで呼び出し側で指示を出すだけにする。

--- src/main/java/com/example/javamemoapp/entity/Memo.java
+++ src/main/java/com/example/javamemoapp/entity/Memo.java
@@ -13,9 +13,27 @@ public class Memo {
 
     private String content;
 
-    public Memo() {
+    protected Memo() {
     }
 
+    public static Memo create(String title, String content) {
+
+        Memo memo = new Memo();
+
+        memo.title = title;
+        memo.content = content;
+
+        return memo;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    /**
+     * getter
+     */
     public Long getId() {
         return id;
     }
@@ -24,12 +42,15 @@ public class Memo {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
+    }
+
+    /**
+     * setter
+     */
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setContent(String content) {
